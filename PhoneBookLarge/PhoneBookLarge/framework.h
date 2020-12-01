5a618b3ae2bfe8406e46acd445602971c5986195
Anastasiya Krasnova <anastasiya.krasnova2017@mail.ru>
2020-12-02 01:20:39 +0300

    GUI

---
+++ PhoneBookLarge/PhoneBookLarge/framework.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+//#include "PhoneBookSmallDll.h"
+#include "resource.h"
+#define WIN32_LEAN_AND_MEAN            
+#include <windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <commctrl.h>
+#include <string>
+#include <vector>
